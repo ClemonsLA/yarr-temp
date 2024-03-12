ee79a6665f188f7ae891ab716948f29265cbf64d
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-12 00:08:34 +0000

    trying to fix components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-import {nextui} from "@nextui-org/react";
+const {nextui} = require("@nextui-org/react");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // ...
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        default: "#FA4616",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   darkMode: "class",
-  plugins: [nextui({
-    themes: {
-      light: {
-        colors: {
-          background: "#FFFFFF", // or DEFAULT
-          foreground: "#FEE7D0", // or 50 to 900 DEFAULT
-          primary: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#FA4616",
-          },
-          danger: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#B00525",
-          },
-          // ... rest of the colors
-        },
-      },
-      dark: {
-        colors: {
-          background: "#000000", // or DEFAULT
-          foreground: "#FEE7D0", // or 50 to 900 DEFAULT
-          primary: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#FA4616",
-          },
-          danger: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#B00525",
-          },
-        },
-        // ... rest of the colors
-      },
-      mytheme: {
-        // custom theme
-        extend: "dark",
-        colors: {
-          primary: {
-            DEFAULT: "#FA4616",
-            foreground: "#000000",
-          },
-          danger: {
-            //... 50 to 900
-            background: "transparent",
-            foreground: "#F6DAC5",
-            DEFAULT: "#490100",
-          },
-          focus: "#BEF264",
-        },
-      },
-    },
-  })],
+  plugins: [nextui()],
 };
-//textColor = "#560900"
