dc7cb768a2476f6fcf0a0178382e387a35556ba7
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-12 00:11:37 +0000

    more cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,6 @@ const nextConfig = {
         serverActions: {
           allowedOrigins: [
             'localhost:3000',
-            '72.20.140.35',
-            'https://72.20.140.35',
-            'rr-v2-q9ybpm1tc-cv-2-nft-main.vercel.app',
-            'https://rr-v2-q9ybpm1tc-cv-2-nft-main.vercel.app',
-            'solid-space-telegram-4j7xw57jjj7hqwv-3000.app.github.dev',
             'https://legendary-guide-qj56g5qr7gqfp56.github.dev/',
           ]
         }
