eb61812d3a44942b31f5c5f1a35eb98ac29fefb1
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-18 04:28:38 +0000

    moved the image to the page instead of client

--- app/imageCreation/page.js
+++ app/imageCreation/page.js
@@ -1,7 +1,7 @@
-import ImageDisplayer from "@/app/components/ImageDisplayer"
+//import ImageDisplayer from "@/app/components/ImageDisplayer"
 import {getURLDataPreload} from '@/app/actions'
-import { cookies } from 'next/headers'
-import { cache } from 'react'
+//import { cookies } from 'next/headers'
+//import { cache } from 'react'
 
 export const maxDuration = 300;
 export const dynamic = 'force-dynamic';
@@ -11,7 +11,22 @@ export default async function imageCreation(){
     //const myBandAidFix = cookies();
     const userFreshURL = await getURLDataPreload();
     return <div className="grid grid-rows-2 ">
-        <ImageDisplayer url={userFreshURL}/>
+        <div className="grid justify-center" style={{ position: 'relative', width: '500px', height: '300px' }}>
+            <image
+                src={userFreshURL}
+                width={1024}
+                height={1792}
+                alt="Dalle Generated Image"
+                style={{
+                    objectFit: "contain",
+                    width: '100%',
+                    height: 'auto',
+                            }}
+                sizes="100vw"
+                priority={true}
+                loading="lazy">
+            </image>
+    </div>
         <button>Paint me!</button>
         <div>{userFreshURL}</div>
     </div>
