581f6194cf754ea78a063520d621853ddb871dfb
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-18 06:48:59 +0000

    trying the next Image comp again

--- app/components/ImageDisplayer.jsx
+++ app/components/ImageDisplayer.jsx
@@ -1,12 +1,12 @@
 'use client'
 
-//import Image from 'next/image';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 export default function ImageDisplayer({url}){
     //const {imageState, setImageState} = useState('https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg')
 
-    return <image
+    return <Image
                 src='https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg'
                 width={1024}
                 height={1792}
@@ -16,5 +16,5 @@ export default function ImageDisplayer({url}){
                     width: '100%',
                     height: 'auto',
                             }}>
-            </image>
+            </Image>
 }
