340eaf46620798bd30dd30ce13dd444ced25a48a
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-18 05:53:06 +0000

    testing out cloudinary image

--- app/components/ImageDisplayer.jsx
+++ app/components/ImageDisplayer.jsx
@@ -4,11 +4,11 @@
 import { useEffect, useState } from 'react';
 
 export default function ImageDisplayer({url}){
-    const {imageState, setImageState} = useState('')
+    const {imageState, setImageState} = useState('https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg')
 
     return <div className="grid justify-center" style={{ position: 'relative', width: '500px', height: '300px' }}>
             <image
-                src={url}
+                src={imageState}
                 width={1024}
                 height={1792}
                 alt="Dalle Generated Image"
--- app/imageCreation/page.js
+++ app/imageCreation/page.js
@@ -1,4 +1,4 @@
-//import ImageDisplayer from "@/app/components/ImageDisplayer"
+import ImageDisplayer from "@/app/components/ImageDisplayer"
 import {getURLDataPreload} from '@/app/actions'
 //import { cookies } from 'next/headers'
 //import { cache } from 'react'
@@ -12,20 +12,7 @@ export default async function imageCreation(){
     const userFreshURL = await getURLDataPreload();
     return <div className="grid grid-rows-2 ">
         <div className="grid justify-center" style={{ position: 'relative', width: '500px', height: '300px' }}>
-            <image
-                src={userFreshURL}
-                width={1024}
-                height={1792}
-                alt="Dalle Generated Image"
-                style={{
-                    objectFit: "contain",
-                    width: '100%',
-                    height: 'auto',
-                            }}
-                sizes="100vw"
-                priority={true}
-                loading="lazy">
-            </image>
+            <ImageDisplayer url={userFreshURL} />
     </div>
         <button>Paint me!</button>
         <div>{userFreshURL}</div>
