0ef2ca0e6315efdd4d22f1967a47294e9ea8407b
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-18 01:15:10 +0000

    re-added a client component with a useEffect

--- app/components/ImageDisplayer.jsx
+++ app/components/ImageDisplayer.jsx
@@ -1,13 +1,16 @@
 'use client'
 
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function ImageDisplayer({url}){
-    const userURL = url;
+    const {imageState, setImageState} = useState('')
+    useEffect(()=> {
+    },[imageState])
 
     return <div className="grid justify-center">
             <Image
-                src={userURL}
+                src={url}
                 width={1024}
                 height={1792}
                 alt="Dalle Generated Image"
@@ -16,7 +19,10 @@ export default function ImageDisplayer({url}){
                     width: '100%',
                     height: 'auto',
                             }}
-                sizes="100vw">
+                sizes="100vw"
+                priority={true}
+                loading='eager'
+                onLoad={(e) => setImageState('loaded')}>
             </Image>
     </div>
 }
--- app/imageCreation/page.js
+++ app/imageCreation/page.js
@@ -1,8 +1,7 @@
-//import ImageDisplayer from "@/app/components/ImageDisplayer"
+import ImageDisplayer from "@/app/components/ImageDisplayer"
 import {getURLDataPreload} from '@/app/actions'
-import Image from 'next/image';
 import { cookies } from 'next/headers'
-import {cache} from 'react'
+import { cache } from 'react'
 
 export const maxDuration = 300; 
 export const dynamic = 'force-dynamic';
@@ -12,20 +11,7 @@ export default async function imageCreation(){
     //const myBandAidFix = cookies();
     const userFreshURL = await getURLDataPreload();
     return <div className="grid grid-rows-2 ">
-        <Image
-                src={userFreshURL}
-                width={1024}
-                height={1024}
-                alt="Dalle Generated Image"
-                priority={true}
-                loading='eager'
-                style={{
-                    objectFit: "contain",
-                    width: '100%',
-                    height: 'auto',
-                            }}
-                sizes="100vw">
-            </Image>
+        <ImageDisplayer url={userFreshURL}/>
         <button>Paint me!</button>
         <div>{userFreshURL}</div>
     </div>
