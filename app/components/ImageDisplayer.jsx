989fc8c24fe7a4af7e58205b13aaac16c1c09fe5
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-03-18 06:52:28 +0000

    next Image works, now trying using useState

--- app/components/ImageDisplayer.jsx
+++ app/components/ImageDisplayer.jsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 export default function ImageDisplayer({url}){
-    //const {imageState, setImageState} = useState('https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg')
+    const {imageState, setImageState} = useState('https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg')
 
     return <Image
-                src='https://res.cloudinary.com/dd7mi7bpp/image/upload/v1690665596/cld-sample-5.jpg'
+                src={imageState}
                 width={1024}
                 height={1792}
                 alt="Dalle Generated Image"
